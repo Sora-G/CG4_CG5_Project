ef729f073329e128885c8bbb6d8085f7e85ea874
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-04-17 11:13:01 +0900

    00_00完成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,33 @@
+#include "KamataEngine.h"
 #include <Windows.h>
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3C_12_スズキ_ソラ");
+
+	// DirectXCommonのインスタンスを取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// メインループ
+	while (true) {
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// 描画開始
+		dxCommon->PreDraw();
+		// 描画処理はここから
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
 	return 0;
 }
