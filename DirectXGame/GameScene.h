b845dc3f23df78da31e064f8f90b10cc0b81ab26
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-04-17 11:20:53 +0900

    GameSceneクラスの追加

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "KamataEngine.h"
+
+class GameScene {
+public: // メンバ関数
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+};
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,3 +1,4 @@
+#include "GameScene.h"
 #include "KamataEngine.h"
 #include <Windows.h>
 
@@ -11,6 +12,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// DirectXCommonのインスタンスを取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
@@ -18,9 +24,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
+		// ゲームシーンの更新処理
+		gameScene->Update();
+
 		// 描画開始
 		dxCommon->PreDraw();
 		// 描画処理はここから
+
+		// ゲームシーンの描画処理
+		gameScene->Draw();
 
 		// 描画終了
 		dxCommon->PostDraw();
@@ -28,6 +40,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの終了処理
 	KamataEngine::Finalize();
+
+	// 解放
+	delete gameScene; // ゲームシーンの解放
+
+	// nullptrの代入
+	gameScene = nullptr;
 
 	return 0;
 }
