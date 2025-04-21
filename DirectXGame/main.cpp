b0e6c20607a95089b77381189da374a28dd251cf
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-04-21 11:14:00 +0900

    RootSignatureを生成する

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,6 +20,23 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList();
 
 
+	//RootSignature作成----------
+	//構造体にデータを用意する
+	D3D12_ROOT_SIGNATURE_DESC descriptorRootSignature{};
+	descriptorRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlog = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(&descriptorRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
+	if (FAILED(hr)) {
+		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
+		assert(false);
+	}
+	//バイナリをもとに生成
+	ID3D12RootSignature* rootSignature = nullptr;
+	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+	assert(SUCCEEDED(hr));
+
+
 
 	// メインループ
 	while (true) {
