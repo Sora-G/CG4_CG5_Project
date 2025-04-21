816b8a3c1ccd56e38de2dd6c94202d8332044c8a
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-04-21 11:19:46 +0900

    InputLayoutの設定を行う

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -37,6 +37,17 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	assert(SUCCEEDED(hr));
 
 
+	//Inputlayoutの設定を行う----------
+	//InputLayout
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
 
 	// メインループ
 	while (true) {
