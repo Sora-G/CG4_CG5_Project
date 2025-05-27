5d83eafd8f91d6315e59354e9beec633e4d07712
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-27 14:57:48 +0900

    shaderクラスの定義(Shader.h) 1/2

--- DirectXGame/Shader.h
+++ DirectXGame/Shader.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <string>	//wstring,string
-#include <d3d12.h>	//ID3DBlob
+
+#include <d3dcompiler.h>
+#include <dxcapi.h>
 
 class Shader {
 public:
 	//シェーダファイルを読み込み、コンパイル済みデータを生成する
 	void Load(const std::wstring& filePath, const std::wstring& shaderModel);
+	void LoadDxc(const std::wstring& filePath, const std::wstring& shaderModel);
 
 	//生成したコンパイル済みデータを取得する
 	ID3DBlob* GetBlob();
