4d1ed5aa04a4c4cb069ac9ab63d17c339e9947a2
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-27 14:50:18 +0900

    string<=>wstring変換関数の作成 3/3

--- DirectXGame/MiscUtility.cpp
+++ DirectXGame/MiscUtility.cpp
@@ -17,4 +17,18 @@ std::wstring ConvertString(const std::string& str) {
 	return result;
 }
 
-std::string ConvertString(const std::wstring& str) { return std::string(); }
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+		NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+		result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
