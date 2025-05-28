a593943063603cdb109f3e305609d7c1d4139fce
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 12:24:06 +0900

    VertexBufferクラスのテスト完

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,6 +4,7 @@
 #include "Shader.h"
 #include "RootSignature.h"
 #include "PipelineState.h"
+#include "VertexBuffer.h"
 
 using namespace KamataEngine;
 
@@ -113,48 +114,52 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//assert(SUCCEEDED(hr));
 
 
-	//VertexResource1を生成する----------
-	//頂点リソース用のヒープの設定
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;//CPUから書き込むヒープ
-	//頂点リソースの設定
-	D3D12_RESOURCE_DESC vertexResourceDesc{};
-	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;//バッファ
-	vertexResourceDesc.Width = sizeof(Vector4) * 3;//リソースのサイズ Vertex4を３頂点分
-	//バッファの場合はこれらを１にする
-	vertexResourceDesc.Height = 1;
-	vertexResourceDesc.DepthOrArraySize = 1;
-	vertexResourceDesc.MipLevels = 1;
-	vertexResourceDesc.SampleDesc.Count = 1;
-	//バッファの場合はこれにする
-	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	//実際に頂点リソースを生成する
-	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;
-	HRESULT hr = dxCommon->GetDevice()->CreateCommittedResource(
-		&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc,
-		D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
-	assert(SUCCEEDED(hr));//上手く行かなかった時は起動できない
+	//VertexBufferの生成
+	VertexBuffer vb;
+	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+
+	////VertexResource1を生成する----------
+	////頂点リソース用のヒープの設定
+	//D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+	//uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;//CPUから書き込むヒープ
+	////頂点リソースの設定
+	//D3D12_RESOURCE_DESC vertexResourceDesc{};
+	//vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;//バッファ
+	//vertexResourceDesc.Width = sizeof(Vector4) * 3;//リソースのサイズ Vertex4を３頂点分
+	////バッファの場合はこれらを１にする
+	//vertexResourceDesc.Height = 1;
+	//vertexResourceDesc.DepthOrArraySize = 1;
+	//vertexResourceDesc.MipLevels = 1;
+	//vertexResourceDesc.SampleDesc.Count = 1;
+	////バッファの場合はこれにする
+	//vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	////実際に頂点リソースを生成する
+	//Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;
+	//HRESULT hr = dxCommon->GetDevice()->CreateCommittedResource(
+	//	&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc,
+	//	D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
+	//assert(SUCCEEDED(hr));//上手く行かなかった時は起動できない
 
 
-	//VertexBufferViewを生成する----------
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	//リソースの先端からアドレスを使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	//使用するリソースのサイズは頂点３つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
-	//１つの頂点のサイズ
-	vertexBufferView.StrideInBytes = sizeof(Vector4);
+	////VertexBufferViewを生成する----------
+	//D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
+	////リソースの先端からアドレスを使う
+	//vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
+	////使用するリソースのサイズは頂点３つ分のサイズ
+	//vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
+	////１つの頂点のサイズ
+	//vertexBufferView.StrideInBytes = sizeof(Vector4);
 
 
 	//Resourceにデータを書き込む----------
 	//頂点リソースにデータを書き込む
 	Vector4* vertexData = nullptr;
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
 	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f};
 	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};
 	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};
 	//頂点リソースのマップを解除する
-	vertexResource->Unmap(0, nullptr);
+	//vertexResource->Unmap(0, nullptr);
 
 
 	// メインループ
@@ -171,7 +176,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		//コマンドを積む
 		commandList->SetGraphicsRootSignature(rs.Get());
 		commandList->SetPipelineState(pipelineState.Get());
-		commandList->IASetVertexBuffers(0, 1, &vertexBufferView);
+		commandList->IASetVertexBuffers(0, 1, vb.GetView());
 		//トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		//頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
