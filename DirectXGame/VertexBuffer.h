524659bd669d9457a8ba43a84807a800f825de4d
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 12:06:36 +0900

    VertexBufferクラスの定義 2/2

--- DirectXGame/VertexBuffer.h
+++ DirectXGame/VertexBuffer.h
@@ -10,4 +10,13 @@ public:
 	//ゲッター
 	ID3D12Resource* Get();//頂点バッファー
 	D3D12_VERTEX_BUFFER_VIEW* GetView();//頂点バッファービュー
+
+	//コンストラクタ
+	VertexBuffer();
+	//デストラクタ
+	~VertexBuffer();
+
+private:
+	ID3D12Resource* vertexBuffer_ = nullptr;//頂点バッファー
+	D3D12_VERTEX_BUFFER_VIEW* vertexBufferView_{};//頂点バッファービュー
 };
