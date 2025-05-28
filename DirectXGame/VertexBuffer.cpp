f9011e075e8981a0b37eee8e52fee9e2c0461d20
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 12:08:42 +0900

    VertexBufferクラスの実装 1/5

--- DirectXGame/VertexBuffer.cpp
+++ DirectXGame/VertexBuffer.cpp
@@ -1,1 +1,12 @@
 #include "VertexBuffer.h"
+#include "KamataEngine.h"
+
+#include <d3dx12.h>
+#include <cassert>
+
+using namespace KamataEngine;
+
+void VertexBuffer::Create(const UINT size, const UINT stride) {
+	// クラス内で取得するために追加
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+}
