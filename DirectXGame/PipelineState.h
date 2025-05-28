67401319aa75612e1bbdbb793985fcb6483721d7
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 11:25:00 +0900

    PipelineStateクラスの定義

--- DirectXGame/PipelineState.h
+++ DirectXGame/PipelineState.h
@@ -1,2 +1,20 @@
 #pragma once
-class PipelineState {};
+
+#include <d3dx12.h>
+
+class PipelineState {
+public:
+	//生成
+	void Create();
+	//ゲッター
+	ID3D12PipelineState* Get();
+
+	//コンストラクタ
+	PipelineState();
+	//デストラクタ
+	~PipelineState();
+
+private:
+	//パイプラインステート
+	ID3D12PipelineState* pipelineState_ = nullptr;
+};
