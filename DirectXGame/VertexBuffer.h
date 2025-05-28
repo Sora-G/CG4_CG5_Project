4cb6ba78001e92bb2a25a520ca6f4ee55d9fb035
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 12:04:16 +0900

    VertexBufferクラスの定義 1/2

--- DirectXGame/VertexBuffer.h
+++ DirectXGame/VertexBuffer.h
@@ -1,2 +1,13 @@
 #pragma once
-class VertexBuffer {};
+
+#include <d3dx12.h>
+
+class VertexBuffer {
+public:
+	//生成
+	void Create(const UINT size,const UINT stride);
+
+	//ゲッター
+	ID3D12Resource* Get();//頂点バッファー
+	D3D12_VERTEX_BUFFER_VIEW* GetView();//頂点バッファービュー
+};
