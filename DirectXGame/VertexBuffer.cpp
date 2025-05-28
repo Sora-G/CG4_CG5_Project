c0eeaaec9db70fddddfaa862207cfad027dea91a
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-28 12:10:12 +0900

    VertexBufferクラスの実装 2/5

--- DirectXGame/VertexBuffer.cpp
+++ DirectXGame/VertexBuffer.cpp
@@ -9,4 +9,20 @@ using namespace KamataEngine;
 void VertexBuffer::Create(const UINT size, const UINT stride) {
 	// クラス内で取得するために追加
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// VertexResource生成
+	// 頂点リソース用のヒープの設定
+	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
+	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // CPUから書き込むヒープ
+	// 頂点リソースの設定
+	D3D12_RESOURCE_DESC vertexResourceDesc{};
+	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
+	vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ Vertex4を３頂点分
+	// バッファの場合はこれらを１にする
+	vertexResourceDesc.Height = 1;
+	vertexResourceDesc.DepthOrArraySize = 1;
+	vertexResourceDesc.MipLevels = 1;
+	vertexResourceDesc.SampleDesc.Count = 1;
+	// バッファの場合はこれにする
+	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 }
