df25ba1ea826f5603262e736e2323330048da533
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-05-29 12:18:15 +0900

    四角形を描画出来た

--- DirectXGame/Model2.cpp
+++ DirectXGame/Model2.cpp
@@ -146,28 +146,24 @@ Model2* Model2::CreateSquare() {
 	indices.resize(kNumIndices);
 
 	//左下
-	vertices[0].pos = {0.0f, 360.0f, 0.0f};
+	vertices[0].pos = {0.0f, 5.0f, 0.0f};
 	vertices[0].uv = {0.0f, 1.0f};
-	vertices[0].normal = vertices[0].pos;
-	vertices[0].normal = MathUtility::Normalize(vertices[0].normal);
+	vertices[0].normal = {0.0f, 0.0f, -1.0f};
 
 	//左上
 	vertices[1].pos = {0.0f, 0.0f, 0.0f};
 	vertices[1].uv = {0.0f, 0.0f};
-	vertices[1].normal = vertices[1].pos;
-	vertices[1].normal = MathUtility::Normalize(vertices[1].normal);
+	vertices[1].normal = {0.0f, 0.0f, -1.0f};
 
 	//右下
-	vertices[2].pos = {640.0f, 360.0f, 0.0f};
+	vertices[2].pos = {5.0f, 5.0f, 0.0f};
 	vertices[2].uv = {1.0f, 1.0f};
-	vertices[2].normal = vertices[2].pos;
-	vertices[2].normal = MathUtility::Normalize(vertices[2].normal);
+	vertices[2].normal = {0.0f, 0.0f, -1.0f};
 
 	//右上
-	vertices[3].pos = {640.0f, 0.0f, 0.0f};
+	vertices[3].pos = {5.0f, 0.0f, 0.0f};
 	vertices[3].uv = {1.0f, 0.0f};
-	vertices[3].normal = vertices[3].pos;
-	vertices[3].normal = MathUtility::Normalize(vertices[3].normal);
+	vertices[3].normal = {0.0f, 0.0f, -1.0f};
 
 	//インデックス
 	indices[0] = 0;	indices[1] = 1;	indices[2] = 2;
