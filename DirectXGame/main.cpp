0eb8d7bdca932ea5143a85df1d2fea58e768c3c5
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-06-17 12:52:43 +0900

    コメントの追加

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -57,10 +57,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//頂点データの準備
 	VertexData vertices[] = {
-	    {1.0f,  1.0f,  0.0f, 1.0f},	//上
-	    {-1.0f,  -1.0f, 0.0f, 1.0f},	//右下
-	    {1.0f, -1.0f, 0.0f, 1.0f},	//左下
-	    {-1.0f,  1.0f,  0.0f, 1.0f}, 
+	    {1.0f,  1.0f,  0.0f, 1.0f}, //  左上
+	    {-1.0f, -1.0f, 0.0f, 1.0f}, //  右下
+	    {1.0f,  -1.0f, 0.0f, 1.0f}, //  左下
+	    {-1.0f, 1.0f,  0.0f, 1.0f}, //  右上
 	};
 
 	//VertexBufferの生成
