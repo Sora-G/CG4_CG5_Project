d28385b8cbbe00b3c1599212a797188a8f8e0c84
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-06-18 12:21:05 +0900

    画面が真っ白になった

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -57,10 +57,10 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//頂点データの準備
 	VertexData vertices[] = {
-	    {1.0f,  1.0f,  0.0f, 1.0f}, //  左上
-	    {-1.0f, -1.0f, 0.0f, 1.0f}, //  右下
-	    {1.0f,  -1.0f, 0.0f, 1.0f}, //  左下
-	    {-1.0f, 1.0f,  0.0f, 1.0f}, //  右上
+	    {1.0f,  1.0f,  0.0f, 1.0f},
+	    {-1.0f, -1.0f, 0.0f, 1.0f},
+	    {1.0f,  -1.0f, 0.0f, 1.0f},
+	    {-1.0f, 1.0f,  0.0f, 1.0f},
 	};
 
 	//VertexBufferの生成
@@ -76,7 +76,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	}
 
 	//頂点インデックスデータの準備
-	uint16_t indices[] = {0, 1, 2, 2, 1, 3};
+	uint16_t indices[] = {0, 2, 1, 1, 3, 0};
 
 	//IndexBuffer(IndexResource,IndexResourceView)の生成
 	IndexBuffer ib;
