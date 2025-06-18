a7e2e2d77234e6062ad08c5c5fbfec0a87e1146a
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-06-18 12:35:27 +0900

    10ページまで書いた

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -58,11 +58,11 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	//頂点データの準備
 	VertexData vertices[] = {
-	    {1.0f,  1.0f,  0.0f, 1.0f},
-	    {-1.0f, -1.0f, 0.0f, 1.0f},
-	    {1.0f,  -1.0f, 0.0f, 1.0f},
-	    {-1.0f, 1.0f,  0.0f, 1.0f},
-	};
+	    {{1.0f, 1.0f, 0.0f, 1.0f},   {1.0f, 1.0f}},
+        {{-1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+        {{1.0f, -1.0f, 0.0f, 1.0f},  {1.0f, 0.0f}},
+        {{-1.0f, 1.0f, 0.0f, 1.0f},  {0.0f, 0.0f}}
+    };
 
 	//VertexBufferの生成
 	VertexBuffer vb;
@@ -127,12 +127,17 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 //引数として、空のpipelineState、RootSignature、頂点シェーダーvs、ピクセルシェーダーpsを参照で受け取る
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
 	//InputLayout
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
 	inputLayoutDesc.NumElements = _countof(inputElementDescs);
