7fa8142a1116d18a5c3097e5c346bc7a2ee88bf8
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-07-17 16:40:36 +0900

    移動実装

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -9,13 +9,37 @@ Player::~Player() {
 
 void Player::Init() {
 	//初期化処理
+	worldTransform_.translation_.y = 0.0f;
 	worldTransform_.Initialize();
-	model_ = Model::CreateSphere();
+	model_ = Model::Create();
 }
 
 void Player::Update() {
 	//更新処理
-	worldTransform_.TransferMatrix();
+	Vector3 move = {};
+	float speed = 0.1f;
+
+	Input* input_ = nullptr;
+	input_ = Input::GetInstance();
+
+	if (input_->PushKey(DIK_W)) {
+		move.y += speed;
+	}
+	if (input_->PushKey(DIK_S)) {
+		move.y -= speed;
+	}
+	if (input_->PushKey(DIK_D)) {
+		move.x += speed;
+	}
+	if (input_->PushKey(DIK_A)) {
+		move.x -= speed;
+	}
+
+	worldTransform_.translation_.x += move.x;
+	worldTransform_.translation_.y += move.y;
+	worldTransform_.translation_.z += move.z;
+
+	worldTransform_.UpdateMatrix();
 }
 
 void Player::Draw(Camera &camera) {
